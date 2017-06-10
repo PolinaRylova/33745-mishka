ca2618fca5a6fc43b2d3807524fca37598e4639b
Polina Rylova <mail@polinarya.ru>
2017-06-10 13:52:00 +0500

    Поправила классы у page-header для отступов под меню на мобильной версии и файл js для этого блока. Добавила оступ для планшетной версии.

--- js/page-header.js
+++ js/page-header.js
@@ -8,7 +8,6 @@ var linkToggle = document.querySelector('.main-nav__link--toggle');
 //При работающем js скрываем класс no-js и делаем меню по умолчанию свёрнутым
 pageHeader.classList.remove('page-header--no-js');
 pageHeader.classList.remove('page-header--menu-opened');//Убираем отступ для раскрытого меню
-pageHeader.classList.add('page-header--menu-closed');//Добавляем отступ для однострочного меню
 itemToggle.classList.remove('main-nav__item--toggle-close');//Меняем крестик
 itemToggle.classList.add('main-nav__item--toggle-open');//на гамбургер и сворачиваем меню*/
 
@@ -18,12 +17,10 @@ linkToggle.addEventListener('click', function(event) {
   if (itemToggle.classList.contains('main-nav__item--toggle-open')) {
     itemToggle.classList.remove('main-nav__item--toggle-open');//меняем гамбургер
     itemToggle.classList.add('main-nav__item--toggle-close');//на крестик и разворачиваем меню
-    pageHeader.classList.remove('page-header--menu-closed');//меняем отступ для однострочного меню
-    pageHeader.classList.add('page-header--menu-opened');//на отступ для раскрытого меню
+    pageHeader.classList.add('page-header--menu-opened');//добавляем отступ для раскрытого меню
   } else {
     itemToggle.classList.add('main-nav__item--toggle-open');
     itemToggle.classList.remove('main-nav__item--toggle-close');
-    pageHeader.classList.add('page-header--menu-closed');
     pageHeader.classList.remove('page-header--menu-opened');
   }
 });
