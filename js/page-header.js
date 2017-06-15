39abe68c1d92373cf475fc688bbec4499f785da2
Polina Rylova <mail@polinarya.ru>
2017-06-15 12:50:07 +0500

    Убрала фиксированное позиционирование у меню и поправила съезжающие в связи с этим блоки

--- js/page-header.js
+++ js/page-header.js
@@ -7,20 +7,20 @@ var linkToggle = document.querySelector('.main-nav__link--toggle');
 
 //При работающем js скрываем класс no-js и делаем меню по умолчанию свёрнутым
 pageHeader.classList.remove('page-header--no-js');
-pageHeader.classList.remove('page-header--menu-opened');//Убираем отступ для раскрытого меню
+pageHeader.classList.add('page-header--menu-closed');//Поднимаем фон для скрытого состояния меню
 itemToggle.classList.remove('main-nav__item--toggle-close');//Меняем крестик
-itemToggle.classList.add('main-nav__item--toggle-open');//на гамбургер и сворачиваем меню*/
+itemToggle.classList.add('main-nav__item--toggle-open');//на гамбургер и сворачиваем меню
 
 //Добавляем обработку события клика по кнопке для раскрытия/сокрытия меню
 linkToggle.addEventListener('click', function(event) {
   event.preventDefault();
   if (itemToggle.classList.contains('main-nav__item--toggle-open')) {
-    itemToggle.classList.remove('main-nav__item--toggle-open');//меняем гамбургер
-    itemToggle.classList.add('main-nav__item--toggle-close');//на крестик и разворачиваем меню
-    pageHeader.classList.add('page-header--menu-opened');//добавляем отступ для раскрытого меню
+    itemToggle.classList.remove('main-nav__item--toggle-open');//удаляем гамбургер
+    itemToggle.classList.add('main-nav__item--toggle-close');//добавляем крестик и разворачиваем меню
+    pageHeader.classList.remove('page-header--menu-closed');//опускаем фон для раскрытого меню
   } else {
-    itemToggle.classList.add('main-nav__item--toggle-open');
-    itemToggle.classList.remove('main-nav__item--toggle-close');
-    pageHeader.classList.remove('page-header--menu-opened');
+    itemToggle.classList.remove('main-nav__item--toggle-close');//убираем крестик
+    itemToggle.classList.add('main-nav__item--toggle-open');//добавляем гамбургер
+    pageHeader.classList.add('page-header--menu-closed');//поднимаем фон
   }
 });
