1c7d4dbeb92564cce5e50a49f0139b089ce67032
Polina Rylova <mail@polinarya.ru>
2017-06-21 13:37:47 +0500

    Поправила всё, что связано со всплывающей формой заказа

--- js/add-to-cart.js
+++ js/add-to-cart.js
@@ -1,10 +1,12 @@
-var orderBtn = document.querySelector(".btn--catalog-order");
+var orderBtn = document.querySelectorAll(".btn--open-modal");
 var modalCart = document.querySelector(".add-to-cart");
 
-orderBtn.addEventListener("click", function(event) {
-  event.preventDefault();
-  modalCart.classList.add("modal-content-show");
-});
+for (var i = 0; i < orderBtn.length; i++) {
+  orderBtn[i].addEventListener("click", function(event) {
+    event.preventDefault();
+    modalCart.classList.add("modal-content-show");
+  });
+}
 
 window.addEventListener("keydown", function(event) {
   if(event.keyCode == 27) {
